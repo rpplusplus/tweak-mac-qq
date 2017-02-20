2b4fcdf67cbf9d360e48f44cdc4e40baf6407c2c
txx <xiaoxuantang@txxs-mac-pro-2.local>
2017-02-20 22:22:08 +0800

    Initial Commit

---
+++ QQHook/QQHook.h
@@ -0,0 +1,13 @@
+//
+//  QQHook.h
+//  QQHook
+//
+//  Created by txx on 20/02/2017.
+//  Copyright © 2017 txx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QQHook : NSObject
+
+@end
---
+++ QQHook/QQHook.m
@@ -0,0 +1,13 @@
+//
+//  QQHook.m
+//  QQHook
+//
+//  Created by txx on 20/02/2017.
+//  Copyright © 2017 txx. All rights reserved.
+//
+
+#import "QQHook.h"
+
+@implementation QQHook
+
+@end
